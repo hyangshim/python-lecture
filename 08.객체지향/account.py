4ec9db34595d248b9f5c407de81fb502f471c6f6
Choi hyang shim <cuixiang523@gamil.com>
2023-04-07 14:27:44 +0900

    ..

---
+++ 08.객체지향/account.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self,ano,owner,balance):
+        self.ano=ano
+        self.owner =owner
+        self.balance =balance
+
+    def deposit(self):
+        pass
+
+    def withdrawo(self):
+        pass
+
+    def __str__(self):
+        pass
+        
