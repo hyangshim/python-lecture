79e3bf13a8b2121618bb41943d809149e6b25fab
Choi hyang shim <cuixiang523@gamil.com>
2023-04-07 17:08:55 +0900

    ..

--- 08.객체지향/account.py
+++ 08.객체지향/account.py
@@ -1,15 +1,27 @@
+
+
+
 class Account:
     def __init__(self,ano,owner,balance):
         self.ano=ano
         self.owner =owner
-        self.balance =balance
+        self.__balance =balance             # 0<= balance<1000000
 
-    def deposit(self):
-        pass
+   
+    
+    def deposit(self,amount):
+        if self.__balance + amount >= 10000000:
+            print('천만원이상은 잔액으로 가져 갈수 없습니다.')
+            return
+        self.__balance += amount 
 
-    def withdrawo(self):
-        pass
+    def withdraw(self,amount):
+        if self.__balance - amount <0:
+            print('잔액이 부족합니다..')
+            return
+        self.__balance -= amount 
+        
 
     def __str__(self):
-        pass
+        return f'계좌번호: {self.ano}, 소유주: {self.owner}, 잔액: {self.__balance:9,d}'
         
